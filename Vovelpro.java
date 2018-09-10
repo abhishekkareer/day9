914a54b02fb8ac7c39db82ac5eeadbdc2036a416
abhishek kareer <abhishekkareer92@gmail.com>
2018-09-11 01:39:06 +0530

    day 9

---
+++ Vovelpro.java
@@ -0,0 +1,10 @@
+ class Vovelpro {
+    public static void main(String args[]) 
+    {
+    	int i=0;
+    String s="hello world";
+  String r=s.replaceAll("[aeiouAEIOU]"," ")
+
+     System.out.println(r.tostring);
+         }
+       }
